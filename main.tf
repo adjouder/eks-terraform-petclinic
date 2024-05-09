cbd10c1cb61050ec2679c32eebfcbafe48fe0422
DJOUDER <djouder@Ordinateur-portable-de-DJOUDER.local>
2024-05-09 21:41:56 +0200

    mise à jour du modul principal

--- main.tf
+++ main.tf
@@ -1,33 +1,52 @@
-###### root/main.tf
+# This Terraform configuration file sets up the infrastructure for the Pet Clinic application.
+# It uses modules to create a VPC, IAM roles, an EKS cluster, and an RDS database.
 provider "aws" {
-  region = var.region
-}
-
-module "eks" {
-  source                  = "./modules/eks"
-  aws_public_subnet       = module.vpc.aws_public_subnet
-  vpc_id                  = module.vpc.vpc_id
-  cluster_name            = "petclinic"
-  endpoint_public_access  = true
-  endpoint_private_access = false
-  public_access_cidrs     = ["0.0.0.0/0"]
-  node_group_name         = "petclinic"
-  scaling_desired_size    = 1
-  scaling_max_size        = 1
-  scaling_min_size        = 1
-  instance_types          = ["t3.large"]
-  key_pair                = "aws-ansible-instances"
+   access_key = var.access_key
+   secret_key = var.secret_key
 }
 
 module "vpc" {
-  source                  = "./modules/vpc"
-  tags                    = "petclinic"
-  instance_tenancy        = "default"
-  vpc_cidr                = "10.0.0.0/16"
-  access_ip               = "0.0.0.0/0"
-  public_sn_count         = 2
-  public_cidrs            = ["10.0.1.0/24", "10.0.2.0/24"]
-  map_public_ip_on_launch = true
-  rt_route_cidr_block     = "0.0.0.0/0"
+  source               = "./modules/vpc"
+  availability_zones   = var.availability_zones
+  cidr_public_subnets  = var.cidr_public_subnets
+  cidr_private_subnets = var.cidr_private_subnets
+  cidr_vpc             = var.cidr_vpc
+  cluster_name    = var.cluster_name
+}
 
+module "natgateway" {
+  source               = "./modules/natgateway"
+  vpc_id                  = module.vpc.vpc_id
+  public_subnet_ids       = module.vpc.public_subnet_ids
+  private_route_table_ids = module.vpc.private_route_table_ids
+}
+
+module "iam" {
+  source          = "./modules/iam"
+  cluster_name    = var.cluster_name
+  oidc_issuer_url = module.eks.oidc_issuer_url
+  aws_account_id  = var.aws_account_id
+}
+
+module "eks" {
+  source               = "./modules/eks"
+  vpc_id               = module.vpc.vpc_id
+  private_subnet_ids   = module.vpc.private_subnet_ids
+  public_subnet_ids   = module.vpc.public_subnet_ids
+  eks_cluster_role_arn = module.iam.eks_cluster_role_arn
+  cluster_name         = var.cluster_name
+  workernodes_iam_role_arn = module.iam.workernodes_iam_role_arn
+  amazon_eks_worker_node_role_policy_attachment = module.iam.amazon_eks_worker_node_role_policy_attachment
+  amazon_eks_cni_role_policy_attachment = module.iam.amazon_eks_cni_role_policy_attachment
+  amazon_ec2_container_registry_read_only_role_policy_attachment = module.iam.amazon_ec2_container_registry_read_only_role_policy_attachment
+  aws_certificate_manager_read_only_eks_role_policy_attachment = module.iam.aws_certificate_manager_read_only_eks_role_policy_attachment
+}
+
+module "rds" {
+  source               = "./modules/rds"
+  petclinic_user       = var.petclinic_user
+  petclinic_mysql_pwd  = var.petclinic_mysql_pwd
+  vpc_id               = module.vpc.vpc_id
+  private_subnet_ids   = module.vpc.private_subnet_ids
+  cidr_private_subnets = var.cidr_private_subnets
 }
