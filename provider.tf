7568c3001f396beff5a538c49f7a611edb4a00b3
adjouder <125672897+adjouder@users.noreply.github.com>
2024-05-19 17:06:25 +0200

    Update provider.tf
    test 19

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
   region ="eu-west-3"
 }
