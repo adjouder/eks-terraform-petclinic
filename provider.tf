808a0e64ead2bf9195eeeebd4e2789459f521be7
adjouder <125672897+adjouder@users.noreply.github.com>
2024-05-19 17:46:11 +0200

    Update provider.tf
    correction des nom de variables

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-    access_key = var.aws_access_key
+    access_key = var.access_key
     region     = "eu-west-3"
-    secret_key = var.aws_secret_key
+    secret_key = var.secret_key
 }
